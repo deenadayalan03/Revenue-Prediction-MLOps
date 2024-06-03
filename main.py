30e9f3a8e4febb7f2e710967c6750f5e23b53cf7
deenadayalan03 <deenadayalan.dms@gmail.com>
2024-06-03 15:43:21 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from src.mlproject import logger
+from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("I'm Deena")
+
+
+
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
